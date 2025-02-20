eb2f87ebd6389b975831dba163fbbd18524f1a14
melissa-epstein <melissa.epstein.toon@gmail.com>
2025-02-20 09:20:12 +0200

    Adding course page

--- js/course-page.js
+++ js/course-page.js
@@ -5,4 +5,23 @@
     signUpButton.addEventListener('click', () => contactForm.scrollIntoView({behavior: 'smooth', block: 'center'}));
     //Running questions
     window.runQuestions('course');
+    //Running modules
+    const addClickListener = (currentModule, nextModule) => {
+        currentModule.querySelector('.program__next').addEventListener('click', () => {
+            currentModule.classList.add('program__module--gotta-leave');
+            nextModule.style.display = 'flex';
+            setTimeout(() => {
+                currentModule.style.display = 'none';
+                currentModule.classList.remove('program__module--gotta-leave');
+            }, 1000);
+        });
+    };
+    const modules = document.querySelectorAll('.program__module');
+    for (let i = 1; i < modules.length; i++) {
+        modules[i].style.display = 'none';
+    }
+    for (let i = 0; i < modules.length - 1; i++) {
+        addClickListener(modules[i], modules[i + 1]);
+    }
+    addClickListener(modules[modules.length - 1], modules[0]);
 })();
